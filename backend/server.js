d7dfb3fb785a0def831c4846bb11090db624edce
Rafal Labunski <rafallabunski@gmail.com>
2025-05-19 20:51:34 +0200

    Backend: dodano konfigurację uploadu i aktualizacje serwera

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const userStatusRoutes = require('./routes/userStatus');
 const adsRoutes = require('./routes/ads');
 const categoriesRoutes = require('./routes/categories');
 const partTypesRoutes = require('./routes/partTypes');
+const imageRoutes = require('./routes/image');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config(); // wczytuje zmienne z .env
@@ -26,6 +27,7 @@ app.use('/api/user-status', userStatusRoutes);
 app.use('/api/ads', adsRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/part-types', partTypesRoutes);
+app.use('/api/images', imageRoutes);
 
 // Start serwera
 const PORT = process.env.PORT || 5000;
